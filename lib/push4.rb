6be12c3c080a85f381cc6a731c928a32a5b933ab
Chris Lucas <chris@chrisjlucas.com>
2013-06-25 15:21:44 -0700

    https fix for ruby 1.8/1.9

--- lib/push4.rb
+++ lib/push4.rb
@@ -58,7 +58,15 @@ module Push4
     end
 
     def send
-      Net::HTTP.post_form(URI(Push4::URL), @data) 
+      uri = URI(URL)
+      
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      
+      req = Net::HTTP::Post.new(uri.to_s)
+      req.set_form_data(@data)
+      
+      http.request(req)
     end
 
     def self.send
